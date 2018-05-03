3c08a0549840dcc4ee3884cd4a60fb5e77182a63
Matt Aurich <mattaurich@gmail.com>
2018-05-03 13:52:48 -0500

    basic react redux

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,13 +1,57 @@
-import PropTypes from 'prop-types';
-import StuffList from './StuffList';
+
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
-      return (
-        <div className="app">
-          <StuffList />
-        </div>
-      );
+    let buttonText;
+    let buttonAction;
+    if (this.props.store.status === 'logged in') {
+      buttonText = 'LogOut';
+      buttonAction = (event) => { this.props.handleLogout(); this.state.value = ''; event.preventDefault(); };
+    } else {
+      buttonText = 'LogIn';
+      buttonAction = (event) => { this.props.handleLogin(this.state); event.preventDefault(); };
     }
+
+    return (
+      <form onSubmit={buttonAction}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value={buttonText} />
+        <h1>Current state is {this.props.store.status + ' as ' + this.props.store.value}</h1>
+      </form>
+    );
+  }
 }
-export default App;
+
+const mapStateToProps = (state) => { 
+  return { store: state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleLogin: state => dispatch({
+      type: 'LOGIN',
+      value: state.value,
+    }),
+    handleLogout: () => dispatch({
+      type: 'LOGOUT',
+    }),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,13 +1,34 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import { createStore } from 'redux';
 import App from './app';
 
-const store = configureStore();
+function auth(state = [], action) {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        value: action.value,
+        status: 'logged in',
+      };
+    case 'LOGOUT':
+      return {
+        value: 'guest',
+        status: 'logged out',
+      }
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  value: 'guest',
+  status: 'logged out',
+};
+const store = createStore(auth, initialState);
 
 render(
   <Provider store={store}>
-      <App />
+    <App />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
